71ab5da122f1602d6509d67a661c6f071841f05d
João Nunes Rios <joaofnr@gmail.com>
2020-12-18 16:17:26 -0300

    Using helper class to manipulate dates

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -12,23 +12,8 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
-        // a few ways to create a date object with input value
-        //let data = new Date(this._inputData.value.split('-'));
-        //let data = new Date(this._inputData.value.replace(/-/g, ','));
-        //let dataValue = this._inputData.value.split('-');
-        //let data = new Date(dataValue[0], dataValue[1]-1, dataValue[2]);
-
-        // we can use ES6's spread operator combined with map and arrow function
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map(
-                (i, ix) => i - ix % 2
-            )
-        );
-
         let negociacao = new Negociacao(
-            data, 
+            DateHelper.textoParaData(this._inputData.value), 
             this._inputQuantidade.value, 
             this._inputValor.value);
 
